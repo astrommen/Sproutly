cedb5e78d1854426b594c21e0eba77e5b3986893
Aja <a.g.mechanix>
2020-10-04 21:49:50 -0400

    configuring postcss with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,12 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        loader: 'postcss-loader',
-        options: {
-          ident: 'postcss',
-          syntax: 'postcss-scss',
-          plugins: () => [
-            require('postcss-import'),
-            require('tailwindcss'),
-            require('autoprefixer'),
-          ]
-        }
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'postcss-loader'
+          }
+        ]
       }
     ]
   }
