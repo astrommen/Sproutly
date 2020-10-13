81ef36a98d3264c73b0f7dca08b6a0b887df351c
Aja <a.g.mechanix>
2020-10-13 10:25:57 -0400

    create & implement onKey + validateUsername funct

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username: string = '';
+  password: string = '';
+  isUsernameValid: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validateUsername(): void {
+    const pattern = RegExp(/^[\w-.]*$/);
+    if (pattern.test(this.username)) {
+      this.isUsernameValid = true;
+    } else {
+      this.isUsernameValid = false;
+    }
+  }
+
+  onKey(event: any, type: string) {
+    if (type === 'username') {
+      this.username = event.target.value;
+      this.validateUsername();
+    } else if (type === 'password') {
+      this.password = event.target.value;
+    }
+  }
 }
