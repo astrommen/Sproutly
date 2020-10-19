4a6a926562aa9bad3c14707de82eb0c805d96943
Aja <a.g.mechanix>
2020-10-18 23:27:38 -0400

    creating our login service & function

---
+++ src/app/services/login/login.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+
+// defining part of our API request
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin': '*'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  // define our url
+  url: any = 'http://localhost:4200/login';
+
+  constructor(
+    // pass httpclient  & router to constructor
+    private http: HttpClient,
+    private router: Router,
+  ) { }
+
+  // login function to handle response; get a token, means user is logged in. Save token to session storage to keep user logged in only during session
+  login(Username: string, Password: string): any {
+    this.http.post(this.url, { Username, Password }, httpOptions).toPromise().then((res: any) => {
+      if (res && res.jwt) {
+        sessionStorage.setItem('jwt', res.jwt);
+      }
+    });
+  }
+}
