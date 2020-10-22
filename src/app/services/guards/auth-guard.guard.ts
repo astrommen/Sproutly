0ce92a63f037a7e32eca184ae1e302f5f2d7cd3e
Aja <a.g.mechanix>
2020-10-21 23:11:16 -0400

    authenication check; auth guard

---
+++ src/app/services/guards/auth-guard.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { LoginService } from './../login/login.service';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+  constructor(
+    public login: LoginService,
+    public router: Router) { }
+  
+    // canActivate(
+    // next: ActivatedRouteSnapshot,
+    // state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    // return true;
+    canActivate(): boolean {
+      if (!this.login.isAuthenticated()) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+  
+}
