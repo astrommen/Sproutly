26d36e80df125715aecf6fac9c40d48b15b6fb0b
Aja <a.g.mechanix>
2020-10-21 23:39:41 -0400

    created dashboard, hooked to authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent} from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuardGuard } from './services/guards/auth-guard.guard'
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardGuard]}
 ];
 
 @NgModule({
