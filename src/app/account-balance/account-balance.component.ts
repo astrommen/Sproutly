598ac7f46ab3e832479df869660ae1eb61523414
Aja <a.g.mechanix@gmail.com>
2020-11-08 23:13:19 -0500

    display user data

--- src/app/account-balance/account-balance.component.ts
+++ src/app/account-balance/account-balance.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-account-balance',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-balance.component.scss']
 })
 export class AccountBalanceComponent implements OnInit {
+  @Input() user: any;
+  date = null;
+  day = null;
+  month = null;
+  year = null;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.date = new Date();
+    this.day = this.date.getDate();
+    this.month = Number(this.date.getMonth()) + 1;
+    this.year = this.date.getFullYear();
   }
 
 }
