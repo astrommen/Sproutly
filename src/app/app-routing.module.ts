ad66cca99eaae7cd78dfbd86ebb67b73f65341fb
Aja <a.g.mechanix@gmail.com>
2020-11-09 22:27:11 -0500

    setting routes and refactor login for register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardGuard } from './services/guards/auth-guard.guard'
 
 import { LoginComponent} from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AuthGuardGuard } from './services/guards/auth-guard.guard'
+import { RegisterComponent } from './register/register.component'
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardGuard]}
 ];
 
