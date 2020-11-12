bf895409de36736804eb22b93e09ab4637b52e43
Aja <a.g.mechanix@gmail.com>
2020-11-12 11:25:08 -0500

    created func to handle inputs

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  username = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onKey(Event: any, type: string) {
+    if (type === 'username') {
+      this.username = Event.target.value;
+    } else if (type === 'email') {
+      this.email = Event.target.value;
+    } else if (type === 'password') {
+      this.password = Event.target.value;
+    } else if (type === 'confirmPassword') {
+      this.confirmPassword = Event.target.value;
+    }
+  }
 }
