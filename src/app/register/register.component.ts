296e839360f0ca02e5dfea714bf514a52cf084ee
Aja <a.g.mechanix@gmail.com>
2020-11-14 00:26:19 -0500

    created validatiom func for register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,10 +10,36 @@ export class RegisterComponent implements OnInit {
   email = '';
   password = '';
   confirmPassword = '';
+  valid = {
+    username: true,
+    email: true,
+    password: true,
+  };
 
   constructor() { }
 
   ngOnInit(): void {}
+
+  validate(type: string): void {
+    const usernamePattern = /^[\w-.]*$/;
+    const emailPattern = /\S+@+\.\S+/;
+
+    if (type === 'username') {
+      if(this.username.length < 5) {
+        this.valid.username = false;
+      } else {
+        this.valid.username = usernamePattern.test(this.username);
+      }
+    } else if (type === 'email') {
+      this.valid.email = emailPattern.test(this.username);
+    } else if (type === ('confirmPassword' || 'password')) {
+      if (this.password !== this.confirmPassword) {
+        this.valid.password = false;
+      } else {
+        this.valid.password = true;
+      }
+    }
+  }
 
   onKey(Event: any, type: string) {
     if (type === 'username') {
@@ -25,5 +51,6 @@ export class RegisterComponent implements OnInit {
     } else if (type === 'confirmPassword') {
       this.confirmPassword = Event.target.value;
     }
+    this.validate(type);
   }
 }
