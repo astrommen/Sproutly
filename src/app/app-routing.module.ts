8e03f78da4eaaa2a14ae56f827113f95f0ae2bc2
Aja <a.g.mechanix@gmail.com>
2020-11-24 20:06:47 -0500

    add user-resolver service and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuardGuard } from './services/guards/auth-guard.guard'
+import { AuthGuardGuard } from './services/guards/auth-guard.guard';
+import { UserResolverService } from './services/user-resolver/user-resolver.service';
+
 
 import { LoginComponent} from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { RegisterComponent } from './register/register.component'
+import { RegisterComponent } from './register/register.component';
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', component: DashboardComponent, canActivate: [AuthGuardGuard]}
+  { path: '', 
+    component: DashboardComponent, 
+    canActivate: [AuthGuardGuard],
+    resolve: { user: UserResolverService }
+    // uses the func from imported class: uses import label from above
+  }
 ];
 
 @NgModule({
